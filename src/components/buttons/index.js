3462c4bdc5444a5e110818f4b0607cca5d42fb7c
Elgun Mehdi <elgun88@gmail.com>
2024-03-03 09:57:49 -0600

    Create file to export all button components

---
+++ src/components/buttons/index.js
@@ -0,0 +1,6 @@
+import AddMoreBtn from './AddMoreBtn/AddMoreBtn';
+import Button from './Button/Button';
+import CloseBtn from './CloseBtn/CloseBtn';
+import RemoveBtn from './RemoveBtn/RemoveBtn';
+
+export { AddMoreBtn, Button, CloseBtn, RemoveBtn };
