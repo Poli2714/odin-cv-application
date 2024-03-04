872cdff122c7a00541f7611c1b215deb536bf90b
Elgun Mehdi <elgun88@gmail.com>
2024-03-03 20:39:56 -0600

    Refactor TextField components
    Moved TextArea and TextInput components into one file,
    and moved them into separater folder with css and test
    files

---
+++ src/components/forms/TextField/TextField.jsx
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+
+import styles from './TextField.module.css';
+
+function TextArea({
+  autofocus = false,
+  maxLength,
+  name,
+  onChange,
+  placeholder = '',
+  rows,
+  value,
+}) {
+  return (
+    <textarea
+      autoFocus={autofocus}
+      className={`${styles.textField} ${styles.textarea}`}
+      id={name}
+      maxLength={maxLength}
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+      rows={rows}
+      value={value}
+    ></textarea>
+  );
+}
+
+TextArea.propTypes = {
+  autofocus: PropTypes.bool,
+  maxLength: PropTypes.number,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  rows: PropTypes.number,
+  value: PropTypes.string,
+};
+
+function TextInput({
+  autofocus = false,
+  disabled = false,
+  name,
+  onChange,
+  pattern,
+  placeholder,
+  value,
+}) {
+  return (
+    <input
+      autoFocus={autofocus}
+      className={styles.textField}
+      disabled={disabled}
+      id={name}
+      name={name}
+      onChange={onChange}
+      pattern={pattern}
+      placeholder={placeholder}
+      type="text"
+      value={value}
+    />
+  );
+}
+
+TextInput.propTypes = {
+  autofocus: PropTypes.bool,
+  disabled: PropTypes.bool,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  pattern: PropTypes.string,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+};
+
+export { TextInput, TextArea };
