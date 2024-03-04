73b0e1c4ee97042795654ec449229c8d94b7b3d5
Elgun Mehdi <elgun88@gmail.com>
2024-03-04 17:13:59 -0600

    Create file to import all layout components from one source

---
+++ src/components/layout/index.js
@@ -0,0 +1,19 @@
+import BtnWrapper from './BtnWrapper/BtnWrapper';
+import CV from './CV/CV';
+import DateInputsWrapper from './DateInputsWrapper/DateInputsWrapper';
+import EditPanel from './EditPanel/EditPanel';
+import EduBtnWrapper from './EduBtnWrapper/EduBtnWrapper';
+import ExpBtnWrapper from './ExpBtnWrapper/ExpBtnWrapper';
+import FormWrapper from './FormWrapper/FormWrapper';
+import InputWrapper from './InputWrapper/InputWrapper';
+
+export {
+  BtnWrapper,
+  CV,
+  DateInputsWrapper,
+  EditPanel,
+  EduBtnWrapper,
+  ExpBtnWrapper,
+  FormWrapper,
+  InputWrapper,
+};
