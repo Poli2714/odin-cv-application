6d57c097b69104ff9372bae330989a43df95846d
Elgun Mehdi <elgun88@gmail.com>
2024-03-05 06:44:37 -0600

    Create formatCVDates utility function and test it

---
+++ src/utils/formatCVDates/formatCVDates.js
@@ -0,0 +1,51 @@
+const convertMonth = month => {
+  switch (month) {
+    case 1:
+      return 'January';
+    case 2:
+      return 'February';
+    case 3:
+      return 'March';
+    case 4:
+      return 'April';
+    case 5:
+      return 'May';
+    case 6:
+      return 'June';
+    case 7:
+      return 'July';
+    case 8:
+      return 'August';
+    case 9:
+      return 'September';
+    case 10:
+      return 'October';
+    case 11:
+      return 'November';
+    case 12:
+      return 'December';
+    default:
+      return month;
+  }
+};
+
+const formatDate = date => {
+  if (date.length < 2) return date;
+  const dateNumeric = date.replace(/\D/g, '');
+  const month = Number(dateNumeric.slice(0, 2));
+  if (dateNumeric.length === 2) return convertMonth(month);
+
+  return `${convertMonth(month)} ${dateNumeric.slice(2)}`;
+};
+
+const formatCVDates = (startDate, endDate) => {
+  if (!startDate.length && !endDate.length) return '';
+  const formattedStartDate = formatDate(startDate);
+  const formattedEndDate = formatDate(endDate);
+
+  if (formattedStartDate === formattedEndDate) return 'Less than a month';
+  if (endDate.length === 0 && startDate.length < 9) return formattedStartDate;
+  return `${formattedStartDate} - ${formattedEndDate}`;
+};
+
+export { convertMonth, formatDate, formatCVDates };
---
+++ src/utils/formatCVDates/formatCVDates.test.js
@@ -0,0 +1,67 @@
+import { describe, it, expect } from 'vitest';
+
+import { convertMonth, formatDate, formatCVDates } from './formatCVDates';
+
+describe('convertMonth', () => {
+  it('converts month to string value', () => {
+    expect(convertMonth(10)).toEqual('October');
+    expect(convertMonth(1)).toEqual('January');
+    expect(convertMonth(0)).toEqual(0);
+  });
+});
+
+describe('formatDate', () => {
+  it("returns original date if date's length is less than 2", () => {
+    expect(formatDate('0')).toEqual('0');
+    expect(formatDate('')).toEqual('');
+  });
+
+  it("returns a string value of month if date's length is between 2 or 5", () => {
+    expect(formatDate('03')).toEqual('March');
+    expect(formatDate('05 / ')).toEqual('May');
+  });
+
+  it('returns correctly formatted month', () => {
+    expect(formatDate('02 / 2024')).toEqual('February 2024');
+    expect(formatDate('08 / 1')).toEqual('August 1');
+    expect(formatDate('10 / 202')).toEqual('October 202');
+  });
+});
+
+describe('formatCVDates', () => {
+  it('returns empty string if no dates are provided', () => {
+    expect(formatCVDates('', '')).toEqual('');
+  });
+
+  it('returns different value if provided dates are the same', () => {
+    expect(formatCVDates('07 / 2020', '07 / 2020')).toMatch(
+      /^less than a month$/i,
+    );
+    expect(formatCVDates('03 / 2000', '03 / 2000')).toMatch(
+      /^less than a month$/i,
+    );
+  });
+
+  it('returns formatted value of first date (still incomplete) without dash character if the second date is not provided', () => {
+    expect(formatCVDates('10 / 198', '')).toEqual('October 198');
+    expect(formatCVDates('01 / 2', '')).toEqual('January 2');
+  });
+
+  it('returns formatted value of first date (completed) with dash character if the second date is not provided', () => {
+    expect(formatCVDates('04 / 1965', '')).toEqual('April 1965 - ');
+    expect(formatCVDates('07 / 2017', '')).toEqual('July 2017 - ');
+  });
+
+  it('returns correct formatted value of provided dates', () => {
+    expect(formatCVDates('04 / 1999', '03 / 2020')).toEqual(
+      'April 1999 - March 2020',
+    );
+    expect(formatCVDates('09 / 2020', '04')).toEqual('September 2020 - April');
+    expect(formatCVDates('08 / 1900', '05 / 200')).toEqual(
+      'August 1900 - May 200',
+    );
+    expect(formatCVDates('02 / 2005', '07 / 2009')).toEqual(
+      'February 2005 - July 2009',
+    );
+  });
+});
