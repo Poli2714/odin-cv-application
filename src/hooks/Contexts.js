63a0613be5eba1bd92207f0f3beadeeebbe909ad
Elgun Mehdi <elgun88@gmail.com>
2024-03-05 06:15:06 -0600

    Create file to store all our contexts

---
+++ src/hooks/Contexts.js
@@ -0,0 +1,78 @@
+import { createContext } from 'react';
+
+const IndexContext = createContext({
+  activeIndex: null,
+  setActiveIndex: () => {},
+});
+
+const KeyContext = createContext({
+  activeKey: {
+    eduActiveKey: null,
+    expActiveKey: null,
+  },
+  setActiveKey: () => {},
+});
+
+const EduErrMsgContext = createContext({
+  showEduErrMessage: false,
+  setShowEduErrMessage: () => {},
+});
+
+const ExpErrMsgContext = createContext({
+  showExpErrMessage: false,
+  setShowExpErrMessage: () => {},
+});
+
+const PersonalInfoContext = createContext({
+  personalInfo: {
+    profilePicture: '',
+    fullName: '',
+    currentJobTitle: '',
+    email: '',
+    mobile: '',
+    city: '',
+  },
+  setPersonalInfo: () => {},
+});
+
+const SummaryContext = createContext({
+  summary: '',
+  setSummary: () => {},
+});
+
+const ExperienceContext = createContext({
+  experiences: [
+    {
+      jobTitle: '',
+      employer: '',
+      jobStartDate: '',
+      jobEndDate: '',
+      jobDescription: '',
+    },
+  ],
+  setExperiences: () => {},
+});
+
+const EducationContext = createContext({
+  educations: [
+    {
+      degree: '',
+      university: '',
+      eduStartDate: '',
+      eduEndDate: '',
+      eduInfo: '',
+    },
+  ],
+  setEducations: () => {},
+});
+
+export {
+  EducationContext,
+  EduErrMsgContext,
+  ExperienceContext,
+  ExpErrMsgContext,
+  IndexContext,
+  KeyContext,
+  PersonalInfoContext,
+  SummaryContext,
+};
