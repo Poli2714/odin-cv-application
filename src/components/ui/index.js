70059e2e596a1ae440d116590631914c390bf8e5
Elgun Mehdi <elgun88@gmail.com>
2024-03-05 06:04:07 -0600

    Create file to import all ui components from one source

---
+++ src/components/ui/index.js
@@ -0,0 +1,6 @@
+import CharacterLimit from './CharacterLimit/CharacterLimit';
+import CVSection from './CVSection/CVSection';
+import CVSectionPart from './CVSectionPart/CVSectionPart';
+import Group from './Group/Group';
+
+export { CharacterLimit, CVSection, CVSectionPart, Group };
