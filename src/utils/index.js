0022041f0b20ead413ea0388b727ac573d823b36
Elgun Mehdi <elgun88@gmail.com>
2024-03-06 05:44:06 -0600

    Create file to export all utility functions

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+import { formatCVDates } from './formatCVDates/formatCVDates';
+import { formatDateInput } from './formatDateInput/formatDateInput';
+import { isEduFormValid, isExpFormValid } from './isFormValid/isFormValid';
+import { isValidEndDate } from './isValidEndDate/isValidEndDate';
+
+export {
+  formatCVDates,
+  formatDateInput,
+  isEduFormValid,
+  isExpFormValid,
+  isValidEndDate,
+};
