12454e84ce8e78f6b2c8d6f30a9a387944db1e2d
Elgun Mehdi <elgun88@gmail.com>
2024-03-06 06:11:36 -0600

    Create file to export all PageHeader components

---
+++ src/views/PageHeader/components/index.js
@@ -0,0 +1,6 @@
+import BrandLogo from './BrandLogo/BrandLogo';
+import DownloadBtn from './DownloadBtn/DownloadBtn';
+import ProfilePicture from './ProfilePicture/ProfilePicture';
+import ShareBtn from './ShareBtn/ShareBtn';
+
+export { BrandLogo, DownloadBtn, ProfilePicture, ShareBtn };
