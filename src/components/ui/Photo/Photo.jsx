383bed02071b31cd006bd9499dd3a8086025ca2f
Elgun Mehdi <elgun88@gmail.com>
2024-03-07 14:22:06 -0600

    Create shared Photo component

---
+++ src/components/ui/Photo/Photo.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+import styles from './Photo.module.css';
+
+function Photo({ size, src }) {
+  return (
+    <div data-testid="photo">
+      <img
+        src={src}
+        alt="Photo"
+        className={styles.photo}
+        height={size}
+        width={size}
+      />
+    </div>
+  );
+}
+
+Photo.proptypes = {
+  size: PropTypes.number,
+  src: PropTypes.string,
+};
+
+export default Photo;
