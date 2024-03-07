1eebc720e290581e45b470df7e2a2309dbd4a1ff
Elgun Mehdi <elgun88@gmail.com>
2024-03-07 14:23:22 -0600

    Update index file in ui component folder

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,5 +2,6 @@ import CharacterLimit from './CharacterLimit/CharacterLimit';
 import CVSection from './CVSection/CVSection';
 import CVSectionPart from './CVSectionPart/CVSectionPart';
 import Group from './Group/Group';
+import Photo from './Photo/Photo';
 
-export { CharacterLimit, CVSection, CVSectionPart, Group };
+export { CharacterLimit, CVSection, CVSectionPart, Group, Photo };
