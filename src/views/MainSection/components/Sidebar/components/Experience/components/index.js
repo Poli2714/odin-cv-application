cd37edcfea84b16f263a32f3e8f6b3062ac3bae0
Elgun Mehdi <elgun88@gmail.com>
2024-03-08 12:22:04 -0600

    Create index file to export all Experience components

---
+++ src/views/MainSection/components/Sidebar/components/Experience/components/index.js
@@ -0,0 +1,7 @@
+import Employer from './Employer/Employer';
+import JobDescription from './JobDescription/JobDescription';
+import JobEndDate from './JobEndDate/JobEndDate';
+import JobStartDate from './JobStartDate/JobStartDate';
+import JobTitle from './JobTitle/JobTitle';
+
+export { Employer, JobDescription, JobEndDate, JobStartDate, JobTitle };
