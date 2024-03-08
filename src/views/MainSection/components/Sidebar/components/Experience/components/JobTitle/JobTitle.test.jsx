ae3370e224d617a1f1aaf840ceacb866be31fd37
Elgun Mehdi <elgun88@gmail.com>
2024-03-08 06:53:30 -0600

    Write test cases for jobTitle component

---
+++ src/views/MainSection/components/Sidebar/components/Experience/components/JobTitle/JobTitle.test.jsx
@@ -0,0 +1,59 @@
+import { expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
+
+import JobTitle from './JobTitle';
+import { ExperienceContext } from 'src/hooks/Contexts';
+
+vi.mock('src/components/layout', () => ({
+  InputWrapper: vi.fn(({ children }) => (
+    <div data-testid="input-wrapper">{children}</div>
+  )),
+}));
+
+vi.mock('src/components/forms', () => ({
+  Label: vi.fn(() => <label htmlFor="jobTitle">Mock label</label>),
+  TextInput: vi.fn(({ onChange, value }) => (
+    <input id="jobTitle" type="text" onChange={onChange} value={value} />
+  )),
+}));
+
+function MockParentComponent({ id }) {
+  const [experiences, setExperiences] = useState({
+    testId: { jobTitle: '' },
+    testId2: { jobTitle: 'Web Developer' },
+  });
+
+  return (
+    <ExperienceContext.Provider value={{ experiences, setExperiences }}>
+      <JobTitle id={id} />
+    </ExperienceContext.Provider>
+  );
+}
+
+test('renders JobTitle', () => {
+  render(<MockParentComponent id="testId" />);
+
+  expect(screen.getByLabelText('Mock label')).toMatchSnapshot();
+});
+
+test('renders jobTitle with initial value', () => {
+  render(<MockParentComponent id="testId2" />);
+
+  expect(screen.getByLabelText('Mock label')).toHaveValue('Web Developer');
+});
+
+test('updates jobTitle value as a user types', async () => {
+  const user = userEvent.setup();
+
+  render(<MockParentComponent id="testId" />);
+  const input = screen.getByLabelText('Mock label');
+
+  await user.type(input, 'Developer');
+  expect(input).toHaveValue('Developer');
+  await user.clear(input);
+  expect(input).toHaveValue('');
+  await user.type(input, 'Frontend Developer');
+  expect(input).toHaveValue('Frontend Developer');
+});
