0dab574936e4a851cfd1ad2bbc516681accc3af4
Elgun Mehdi <elgun88@gmail.com>
2024-03-07 19:14:55 -0600

    Update Label component to accept className

--- src/components/forms/Label/Label.jsx
+++ src/components/forms/Label/Label.jsx
@@ -2,15 +2,20 @@ import PropTypes from 'prop-types';
 
 import styles from './Label.module.css';
 
-function Label({ labelFor, labelName }) {
+function Label({ clName = '', labelFor, labelName }) {
   return (
-    <label data-testid="label" htmlFor={labelFor} className={styles.labelName}>
+    <label
+      data-testid="label"
+      htmlFor={labelFor}
+      className={`${styles.labelName}${clName && ' ' + clName}`}
+    >
       {labelName}
     </label>
   );
 }
 
 Label.propTypes = {
+  clName: PropTypes.string,
   labelFor: PropTypes.string,
   labelName: PropTypes.string,
 };
