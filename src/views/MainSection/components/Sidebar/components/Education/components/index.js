097abd5800eb1a418e0efa24882a7757adde3cf8
Elgun Mehdi <elgun88@gmail.com>
2024-03-09 09:09:31 -0600

    Create index file to export all Educaion components

---
+++ src/views/MainSection/components/Sidebar/components/Education/components/index.js
@@ -0,0 +1,7 @@
+import Degree from './Degree/Degree';
+import EduEndDate from './EduEndDate/EduEndDate';
+import EduInfo from './EduInfo/EduInfo';
+import EduStartDate from './EduStartDate/EduStartDate';
+import University from './University/University';
+
+export { Degree, EduEndDate, EduInfo, EduStartDate, University };
