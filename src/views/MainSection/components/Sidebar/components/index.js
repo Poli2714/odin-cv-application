aebec7cbdb94b9b029d77431c07f5eada16c74bb
Elgun Mehdi <elgun88@gmail.com>
2024-03-10 11:15:06 -0600

    Create index file to export all Sidebar components

---
+++ src/views/MainSection/components/Sidebar/components/index.js
@@ -0,0 +1,25 @@
+import EduAddMoreBtn from './EduAddMoreBtn/EduAddMoreBtn';
+import Education from './Education/Education';
+import EduRemoveBtn from './EduRemoveBtn/EduRemoveBtn';
+import ExpAddMoreBtn from './ExpAddMoreBtn/ExpAddMoreBtn';
+import Experience from './Experience/Experience';
+import ExpRemoveBtn from './ExpRemoveBtn/ExpRemoveBtn';
+import InvalidEduFormMsg from './InvalidEduFormMsg/InvalidEduFormMsg';
+import InvalidExpFormMsg from './InvalidExpFormMsg/InvalidExpFormMsg';
+import PersonalInformation from './PersonalInformation/PersonalInformation';
+import SidebarMenu from './SidebarMenu/SidebarMenu';
+import Summary from './Summary/Summary';
+
+export {
+  EduAddMoreBtn,
+  Education,
+  EduRemoveBtn,
+  ExpAddMoreBtn,
+  Experience,
+  ExpRemoveBtn,
+  InvalidEduFormMsg,
+  InvalidExpFormMsg,
+  PersonalInformation,
+  SidebarMenu,
+  Summary,
+};
