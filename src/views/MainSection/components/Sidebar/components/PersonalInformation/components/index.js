5b06c6da725372a77bde6d40a435d58e48f1ef05
Elgun Mehdi <elgun88@gmail.com>
2024-03-10 11:09:43 -0600

    Create index file to export all PersonalInfo components

---
+++ src/views/MainSection/components/Sidebar/components/PersonalInformation/components/index.js
@@ -0,0 +1,15 @@
+import CurrentJobTitle from './CurrentJobTitle/CurrentJobTitle';
+import Email from './Email/Email';
+import FullName from './FullName/FullName';
+import Location from './Location/Location';
+import MobileNumber from './MobileNumber/MobileNumber';
+import UploadPicture from './UploadPicture/UploadPicture';
+
+export {
+  CurrentJobTitle,
+  Email,
+  FullName,
+  Location,
+  MobileNumber,
+  UploadPicture,
+};
