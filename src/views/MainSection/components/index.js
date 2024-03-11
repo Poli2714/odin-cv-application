404a8834032d2c41efaf3e50764ffcf6ee8d57ca
Elgun Mehdi <elgun88@gmail.com>
2024-03-11 06:01:33 -0600

    Create index file to export all MainSection components

---
+++ src/views/MainSection/components/index.js
@@ -0,0 +1,6 @@
+import CVEducation from './CVEducation/CVEducation';
+import CVExperience from './CVExperience/CVExperience';
+import CVHeader from './CVHeader/CVHeader';
+import Sidebar from './Sidebar/Sidebar';
+
+export { CVEducation, CVExperience, CVHeader, Sidebar };
